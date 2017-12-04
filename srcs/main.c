bd2c5ded0c1a91edcf990260519051c3cebc516f
Alicia BERRAHMA <aberrahm@e2r4p2.42.fr>
2017-12-04 03:20:21 +0100

    finish

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aberrahm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 01:44:25 by aberrahm          #+#    #+#             */
-/*   Updated: 2017/12/04 01:03:27 by aberrahm         ###   ########.fr       */
+/*   Updated: 2017/12/04 03:18:45 by aberrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,9 @@ int				main(int ac, char **av)
 {
 	int		fd;
 	t_all	img1;
+	int		i;
 
+	i = 0;
 	if (ac > 1 && av[1])
 	{
 		if ((fd = open(av[1], O_RDONLY)) < 0)
